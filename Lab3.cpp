10da18728e73053140a02369479bc49dda5fe981
User <User@DESKTOP-KOBUB7D>
2023-03-25 18:20:32 +0200

    1

--- Lab3.cpp
+++ Lab3.cpp
@@ -5,11 +5,6 @@
 #include <math.h>
 #include <clocale>
 #include <complex>
-typedef complex <double> ComplexDouble;
-#define _RE 0
-#define _IM 1
-
-
 using namespace std;
 
 /*
@@ -51,116 +46,116 @@ class Icosahedron {
 public:
     Icosahedron() : a(5), b(1), c(2019) {}
     Icosahedron(int ai) : a(ai), b(1), c(2019) {}
-    Icosahedron(double bi) : a(5), c(2019) {if (bi > 0) b = bi; else b = 1;}
-    Icosahedron(float ci) : a(20), b(1) { if (ci > 0) c = ci; else c = 2000;}
+    Icosahedron(double bi) : a(5), c(2019) { if (bi > 0) b = bi; else b = 1; }
+    Icosahedron(float ci) : a(20), b(1) { if (ci > 0) c = ci; else c = 2000; }
 
-         double getA() const
-         {
-            return a;
-         }
-     
-        void setA(int a)
+    double getA() const
+    {
+        return a;
+    }
+
+    void setA(int a)
+    {
+        if (a < 1 || a > 31)
         {
-            if (a < 1 || a > 31)
-            {
-                cout << " Error set a \n";
-                return;
-            }
-            this->a = a;
+            cout << " Error set a \n";
+            return;
         }
+        this->a = a;
+    }
 
 
-        double getB() const
+    double getB() const
+    {
+        return b;
+    }
+
+    void setB(int b)
+    {
+        if (b < 1 || b > 12)
         {
-            return b;
+            cout << " Error set b \n";
+            return;
         }
+        this->b = b;
+    }
 
-        void setB(int b)
+
+    double getC() const
+    {
+        return c;
+    }
+
+    void setC(int c)
+    {
+        if (c < 1000 || c > 2025)
         {
-            if (b < 1 || b > 12)
-            {
-                cout << " Error set b \n";
-                return;
-            }
-            this->b = b;
+            cout << " Error set c \n";
+            return;
         }
-
-
-        double getC() const
-        {
-            return c;
-        }
-
-        void setC(int c)
-        {
-            if (c < 1000 || c > 2025)
-            {
-                cout << " Error set c \n";
-                return;
-            }
-            this->c = c;
-        }
+        this->c = c;
+    }
 
 
 
-        double A() {
-            cout << "A: ";
-            cin >> a;
-            return a;
-        }
+    double A() {
+        cout << "A: ";
+        cin >> a;
+        return a;
+    }
 
-        double B() {
-            cout << "\nB: ";
-            cin >> b;
-            return b;
-        }
+    double B() {
+        cout << "\nB: ";
+        cin >> b;
+        return b;
+    }
 
-        double C() {
-            cout << "\nC: ";
-            cin >> c;
-            return c;
-        }
-
-
-
-        void printInfo()
-        {
-            cout << "\n a= " << a << " b = " << b << " c = " << c << "\n";
-        }
+    double C() {
+        cout << "\nC: ";
+        cin >> c;
+        return c;
+    }
 
 
 
-        void printInfo1()
-        {
-            cout << "\n";
-            if ((a == 1)||(a == 2)||(a == 3)||(a == 4) || (a == 5) || (a == 6) || (a == 7) || (a == 8) || (a == 9)) { cout << "0"; }
-            cout << a << ".";
-            if ((b == 1) || (b == 2) || (b == 3) || (b == 4) || (b == 5) || (b == 6) || (b == 7) || (b == 8) || (b == 9)) { cout << "0"; }
-            cout << b << "." << c << "\n";
-        }
-        void printInfo2()
-        {
-            string t;
-            if (b == 1) { t = "january"; }
-            if (b == 2) { t = "february"; }
-            if (b == 3) { t = "march"; }
-            if (b == 4) { t = "april"; }
-            if (b == 5) { t = "may"; }
-            if (b == 6) { t = "june"; }
-            if (b == 7) { t = "july"; }
-            if (b == 8) { t = "august"; }
-            if (b == 9) { t = "september"; }
-            if (b == 10) { t = "october"; }
-            if (b == 11) { t = "november"; }
-            if (b == 12) { t = "december"; }
+    void printInfo()
+    {
+        cout << "\n a= " << a << " b = " << b << " c = " << c << "\n";
+    }
 
-            cout << "\n" << a << " " << t << " " << c << " year" << "\n";
-        }
-    };
+
+
+    void printInfo1()
+    {
+        cout << "\n";
+        if ((a == 1) || (a == 2) || (a == 3) || (a == 4) || (a == 5) || (a == 6) || (a == 7) || (a == 8) || (a == 9)) { cout << "0"; }
+        cout << a << ".";
+        if ((b == 1) || (b == 2) || (b == 3) || (b == 4) || (b == 5) || (b == 6) || (b == 7) || (b == 8) || (b == 9)) { cout << "0"; }
+        cout << b << "." << c << "\n";
+    }
+    void printInfo2()
+    {
+        string t;
+        if (b == 1) { t = "january"; }
+        if (b == 2) { t = "february"; }
+        if (b == 3) { t = "march"; }
+        if (b == 4) { t = "april"; }
+        if (b == 5) { t = "may"; }
+        if (b == 6) { t = "june"; }
+        if (b == 7) { t = "july"; }
+        if (b == 8) { t = "august"; }
+        if (b == 9) { t = "september"; }
+        if (b == 10) { t = "october"; }
+        if (b == 11) { t = "november"; }
+        if (b == 12) { t = "december"; }
+
+        cout << "\n" << a << " " << t << " " << c << " year" << "\n";
+    }
+};
 
 int A()
 {
-    
+
     Icosahedron obj;
     obj.printInfo();
     int in_a, in_b, in_c;
@@ -271,39 +266,26 @@ int A()
 }
 
 
+typedef complex <double> ComplexDouble;
+#define _RE 0
+#define _IM 1
+
 class ComplexVector
 {
     ComplexDouble* v;
-    int num;
-    int state;
-
+    int num;   // default num=2
+    int state = 0;
 public:
-    ComplexVector() : v(NULL), num(0), state(0) {}  // 1
-    ComplexVector(int n);  // 2
-    ComplexVector(int n, ComplexDouble&);  // 3
-    ComplexVector(int n, ComplexDouble*);  // 4 (копій)
-    ComplexVector(const ComplexVector& s);  // 4 (присвоєнь)
-    ComplexVector& operator=(const ComplexVector& s);  // ?!
-    ~ComplexVector() {   // 5 (пам'ять)
-        cout << " del vec";
+    ComplexVector() : ComplexVector(2) {}
+    ComplexVector(int n);
+    ComplexVector(int n, ComplexDouble&);
+    ComplexVector(int n, ComplexDouble*);
+    ComplexVector(const ComplexVector& s);
+    ComplexVector& operator=(const ComplexVector& s);
+    ~ComplexVector() {
+        std::cout << " del vec";
         if (v) delete[] v;
     }
-
-
-    /*
-  +  - визначити функцію, яка присвоює елементу масиву деяке значення (параметр за замовчуванням);
-  +  - функцію яка одержує деякий елемент масиву;
-  +  - віднімання, які здійснюють ці арифметичні операції з даними цього класу, 
-  +  - множення на ціле типу short;
-  +  - визначити функції порівняння: 
-        - більше, 
-        - менше або рівно, які повертають true або false
-
-    У змінну стани встановлювати код помилки, коли не вистачає пам'яті, виходить за межі масиву. 
-    Передбачити можливість підрахунку числа об'єктів даного типу. 
-    Написати програму тестуваннявсіх можливостей цього класу.
-
-    */
 
     ComplexVector(int n, int a); // функцію, яка присвоює елементу масиву деяке значення (параметр за замовчуванням)
     ComplexVector(int n, int b); // функцію яка одержує деякий елемент масиву
@@ -313,47 +295,46 @@ public:
     bool Less(ComplexVector& s); // менше
     bool Equal(ComplexVector& s); // рівно
 
-
-
-    void Output(); // друк
+    void Output();
     void Input();
-    ComplexVector Add(ComplexVector& b); // додавання
+    ComplexVector Add(ComplexVector& b);
+
 };
 
-
 ComplexVector::ComplexVector(int n) {
-    if (n <= 0) { v = NULL; num = 0; state = -1; cout << " Vec --> 0 "; }
+    if (n <= 0)    n = 2;  // default num =2;
     num = n;
     v = new ComplexDouble[n];
     for (int i = 0; i < n; i++) {
         v[i] = 0.0;
-        //v[i]._Val[_RE]=0.0; v[i]._Val[_IM]=0.0;
+        //v[i]._Val[_RE]=0.0; v[i]._Val[_IM]=0.0;  
     }
+
 }
-ComplexVector::ComplexVector(int n, ComplexDouble& b) {
-    if (n <= 0) { v = NULL; num = 0; state = -1; cout << " Vec --> 0 "; }
-    num = n;
-    v = new ComplexDouble[n];
-    for (int i = 0; i < n; i++) {
+ComplexVector::ComplexVector(int n, ComplexDouble& b) : ComplexVector(n) {
+    for (int i = 0; i < num; i++) {
         v[i] = b;
-        //v[i]._Val[_RE]=0.0; v[i]._Val[_IM]=0.0;
+        //v[i]._Val[_RE]=0.0; v[i]._Val[_IM]=0.0;  
     }
 }
-ComplexVector::ComplexVector(int n, ComplexDouble* p) {
-    if (n <= 0 || p == NULL) { v = NULL; num = 0; state = -1; cout << " Vec --> 0 "; }
-    num = n;
-    v = new ComplexDouble[n];
-    for (int i = 0; i < n; i++) {
-        v[i] = p[i];
-    }
+
+ComplexVector::ComplexVector(int n, ComplexDouble* p) : ComplexVector(n) {
+    if (p != nullptr)
+        for (int i = 0; i < num; i++)
+            v[i] = p[i];
+
 }
+
 ComplexVector::ComplexVector(const ComplexVector& s) {
+    // if (this == &s) return;  // the expression is used in the old standard
     num = s.num;
     v = new ComplexDouble[num];
     state = 0;
-    for (int i = 0; i < num; i++) v[i] = s.v[i];
+    for (int i = 0; i < num; i++)   v[i] = s.v[i];
 }
+
 ComplexVector& ComplexVector::operator=(const ComplexVector& s) {
+
     if (num != s.num)
     {
         if (v) delete[] v;
@@ -361,51 +342,19 @@ ComplexVector& ComplexVector::operator=(const ComplexVector& s) {
         v = new ComplexDouble[num];
         state = 0;
     }
-    for (int i = 0; i < num; i++) v[i] = s.v[i];
+    for (int i = 0; i < num; i++)   v[i] = s.v[i];
     return *this;
 }
-void ComplexVector::Input() {
-    if (num == 0) {
-        if (v) delete[] v;
-        do {
-            cout << "Input size Vec\n";
-            cin >> num;
-        } while (num <= 0);
-        v = new ComplexDouble[num];
-    }
-    for (int i = 0; i < num; i++) {
-        cout << " v [ " << i << " ] real img "; cin >> v[i] >> v[i]._Val[_IM];
-    }
-}
-void ComplexVector::Output() {
-    if (num != 0) {
-        for (int i = 0; i < num; i++) {
-            cout << " v [ " << i << " ] " << v[i] << '\t';
-            cout << endl;
-        }
-    }
-}
-ComplexVector ComplexVector::Add(ComplexVector& b) {
-    int tnum;
-    tnum = num < b.num ? num : b.num;
-    if (tnum >= 0) {
-        ComplexVector tmp(tnum);
-        for (int i = 0; i < tnum; i++) tmp.v[i] = v[i] + b.v[i];
-        return tmp;
-    }
-    return ComplexVector(0);
-}
 
 
-//
-ComplexVector::ComplexVector(int n, int a){
+ComplexVector::ComplexVector(int n, int a) {
     a = 1; // параметр за замовчуванням
 
     cout << "V: ";
     for (int i = 0; i < n; i++) {
         cout << v[i] << " ";
     }
-    
+
     cout << "Input position of element: ";
     int p;
     cin >> p;
@@ -452,7 +401,7 @@ ComplexVector ComplexVector::Mult(ComplexVector& b) {
         for (int i = 0; i < tnum; i++) tmp.v[i] = v[i] * s;
         return tmp;
     }
-  
+
     return ComplexVector(0);
 }
 
@@ -473,6 +422,55 @@ bool ComplexVector::Equal(ComplexVector& s) {
     if (num == s.num && state == s.state) return true;
     return false;
 }
+
+
+
+void ComplexVector::Input() {
+    int in_num = 0;
+    do {
+        cout << "Input size Vec\n";
+        cin >> in_num;
+    } while (in_num <= 0);
+    if (num != in_num) {
+        num = in_num;
+        if (v) delete[] v;
+        v = new ComplexDouble[num];
+    }
+    for (int i = 0; i < num; i++) {
+
+#if defined(_MSC_VER)
+        cout << " v [ " << i << " ] real img  "; cin >> v[i] >> v[i]._Val[_IM];
+#else 
+        double re, im;
+        cout << " v [ " << i << " ] real img  "; cin >> re >> im;
+        v[i].real(re);
+        v[i].imag(im);
+#endif		
+
+
+    }
+}
+
+void ComplexVector::Output() {
+    if (num != 0) {
+        for (int i = 0; i < num; i++) {
+            cout << " v [ " << i << " ]   " << v[i] << '\t';
+            cout << endl;
+        }
+    }
+}
+
+ComplexVector ComplexVector::Add(ComplexVector& b) {
+    int tnum;
+    tnum = num < b.num ? num : b.num;
+    if (tnum >= 0) {
+        ComplexVector tmp(tnum);
+        for (int i = 0; i < tnum; i++) tmp.v[i] = v[i] + b.v[i];
+        return tmp;
+    }
+    return ComplexVector(1);
+}
+
 
 int B()
 {
@@ -515,10 +513,8 @@ int B()
     VecObj1 = VecObj.Add(VecObj2);
     VecObj1.Output();
 
-    return 0;
+    return 1;
 }
-
-
 
 
 
